2bb769a392db4df833b34c3162504c9ded05c8a4
James C. Bevier <jbev@worker.jbsys.com>
2021-07-08 12:23:51 -0700

    NBC: Add cd command to NBC 1.4.

---
+++ cmd_cd/cd.m
@@ -0,0 +1,22 @@
+ 
+ 
+     CD(C)		       MAKE4MPX			 CD(C)
+ 
+ 
+     Name
+	  cd - Changes working directory.
+ 
+     Syntax
+	  cd [directory]
+ 
+     Description
+	  If specified,	directory becomes the new working directory;
+	  otherwise the	value /system/system/ is used.  The process
+    	  must have search (execute) permission in all directories
+    	  (components) specified in the full pathname of directory.
+ 
+     See Also
+	  pwd, sh, chdir
+ 
+     (printed 4/6/94)
+ 
